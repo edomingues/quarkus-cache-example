5f173793d5f1bbf83169f91be00878c6fbef0c71
Edgar Domingues <edgar.domingues@talkdesk.com>
2021-01-23 22:01:55 +0000

    Quarkus cache example

---
+++ src/test/java/com/edgar/domingues/examples/cache/WeatherForecastResourceTest.java
@@ -0,0 +1,21 @@
+package com.edgar.domingues.examples.cache;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@QuarkusTest
+public class WeatherForecastResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/weather")
+                .then()
+                .statusCode(200)
+                .body(containsString("dailyForecasts"));
+    }
+
+}
